5fb37def9bfd65465fbc9177a3b901245e3cbff4
Fabien Roy <fabienroy28@gmail.com>
2022-12-02 19:56:53 -0500

    feat: map list of articles

--- apps/web/src/domain/articles/components/articles_page.rs
+++ apps/web/src/domain/articles/components/articles_page.rs
@@ -1,8 +1,19 @@
 use yew::prelude::*;
 
+use crate::domain::articles::mocks::articles::get_mock_articles;
+
 #[function_component(ArticlesPage)]
 pub fn articles_page() -> Html {
+    let articles = get_mock_articles();
+
     html! {
-        <p>{ "Articles" }</p>
+        <>
+            <h2>{ "Articles" }</h2>
+            { for articles.into_iter().map(|article| {
+                html! {
+                    <p>{ article.title }</p>
+                }
+            })}
+        </>
     }
 }
