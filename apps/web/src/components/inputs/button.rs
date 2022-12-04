8e980f5758bc39001f6d9bf253788367ecaefbcb
Fabien Roy <fabienroy28@gmail.com>
2022-12-04 13:08:14 -0500

    chore: enhance default values for button

--- apps/web/src/components/inputs/button.rs
+++ apps/web/src/components/inputs/button.rs
@@ -14,8 +14,10 @@ pub enum ButtonVariant {
 #[derive(Properties, Clone, PartialEq)]
 pub struct Props {
     pub text: String,
-    pub disabled: Option<bool>,
-    pub variant: Option<ButtonVariant>,
+    #[prop_or(false)]
+    pub disabled: bool,
+    #[prop_or(ButtonVariant::Contained)]
+    pub variant: ButtonVariant,
     pub onclick: Option<Callback<Event>>,
 }
 
@@ -92,13 +94,7 @@ fn get_colors(variant: ButtonVariant, disabled: bool, colors: Colors) -> ButtonC
 #[styled_component(Button)]
 pub fn button(props: &Props) -> Html {
     let theme_context = use_theme_context();
-    // TODO: Can we use "derivative" or similar to handle default values?
-    let disabled = props.disabled.unwrap_or(false);
-    let variant = match props.variant.clone() {
-        Some(variant) => variant,
-        None => ButtonVariant::Contained,
-    };
-    let colors = use_memo(|colors| get_colors(variant, disabled, colors.clone()), theme_context.theme.colors.clone());
+    let colors = use_memo(|colors| get_colors(props.variant.clone(), props.disabled, colors.clone()), theme_context.theme.colors.clone());
 
     html! {
         <button class={css!(
@@ -129,7 +125,7 @@ pub fn button(props: &Props) -> Html {
             "#,
             font_weight_regular = theme_context.theme.fonts.weights.regular.clone(),
             font_size_s = theme_context.theme.fonts.sizes.s.clone(),
-            cursor = if disabled { "not-allowed".to_string() } else { "pointer".to_string() },
+            cursor = if props.disabled { "not-allowed".to_string() } else { "pointer".to_string() },
             border = colors.border.clone(),
             background = colors.background.clone(),
             on_background = colors.on_background.clone(),
@@ -141,7 +137,7 @@ pub fn button(props: &Props) -> Html {
             spacing_xs = theme_context.theme.spacings.xxs.clone(),
         )}
             onclick={props.onclick.clone()}
-            {disabled}
+            disabled={props.disabled}
         >
             { props.text.clone() }
         </button>
